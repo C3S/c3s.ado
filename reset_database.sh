78277a804c2d97ae25ce220a34a19b820a21134e
Alexander Blum <alexander.blum@c3s.cc>
2016-02-19 17:54:27 +0100

    generalizes reset_database.sh

--- reset_database.sh
+++ reset_database.sh
@@ -6,27 +6,25 @@ echo "= this script stops, destroys, rebuilds, restarts the c3s.ado setup ="
 echo "====================================================================="
 echo `date +%Y-%m-%d:%H:%M:%S`
 
-cd /srv/web/alpha.adore-music.com/c3s.ado
-
 # stop docker containers
 echo -e "\n== stop docker containers"
-/usr/local/bin/docker-compose stop
+docker-compose stop
 
 # delete database
 echo -e "\n== delete database"
-/usr/local/bin/docker-compose run portal ado-do db-delete c3s
+docker-compose run portal ado-do db-delete c3s
 echo $?
 echo "0 means success, 1 and above mean failure!"
 
 # re-create database
 echo -e "\n== re-create database"
-/usr/local/bin/docker-compose run tryton ado-do db-demo-setup c3s &> /dev/null
+docker-compose run tryton ado-do db-demo-setup c3s &> /dev/null
 echo $?
 echo "0 means success, 1 and above mean failure!"
 
 # start docker containers
 echo -e "\n== start docker containers"
-/usr/local/bin/docker-compose up -d
+docker-compose up -d
 
 echo `date +%Y-%m-%d:%H:%M:%S`
 echo "====================================================================="
